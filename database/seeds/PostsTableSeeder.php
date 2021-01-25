747b404e2691811f0cb880de1c2f0e64ae0d43a4
Mikko Paulo Guevarra <emphigie10@gmail.com>
2021-01-26 01:36:28 +0800

    goods

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -13,12 +13,16 @@ class PostsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $new_post_obj = new Post();
-        $new_post_obj->name = $faker->firstName();
-        $new_post_obj->lastname = $faker->lastName();
-        $new_post_obj->title = $faker->sentence(3);
-        $new_post_obj->subtitle = $faker->sentence(3);
-        $new_post_obj->content = $faker->paragraph(2);
-        $new_post_obj->save();
+        for ($i=0; $i < 10 ; $i++) {
+
+            $new_post_obj = new Post();
+            $new_post_obj->name = $faker->firstName();
+            $new_post_obj->lastname = $faker->lastName();
+            $new_post_obj->title = $faker->sentence(3);
+            $new_post_obj->subtitle = $faker->sentence(3);
+            $new_post_obj->content = $faker->paragraph(2);
+            $new_post_obj->category = $faker->paragraph(2);
+            $new_post_obj->save();
+        }
     }
 }
