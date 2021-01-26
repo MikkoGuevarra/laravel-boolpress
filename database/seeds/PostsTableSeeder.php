9d9d4ccc7e3a722cd4090d34acbe8247b4d3a79f
Mikko Paulo Guevarra <emphigie10@gmail.com>
2021-01-26 15:43:22 +0800

    tried other fakers

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -18,10 +18,10 @@ class PostsTableSeeder extends Seeder
             $new_post_obj = new Post();
             $new_post_obj->name = $faker->firstName();
             $new_post_obj->lastname = $faker->lastName();
-            $new_post_obj->title = $faker->sentence(3);
+            $new_post_obj->title = $faker->word();
             $new_post_obj->subtitle = $faker->sentence(3);
             $new_post_obj->content = $faker->paragraph(2);
-            $new_post_obj->category = $faker->paragraph(2);
+            $new_post_obj->category = $faker->word();
             $new_post_obj->save();
         }
     }
