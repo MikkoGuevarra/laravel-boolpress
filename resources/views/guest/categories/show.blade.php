10371445615dbe7d1acc93fc9c12aa830e1d1894
Mikko Paulo Guevarra <emphigie10@gmail.com>
2021-01-31 19:42:16 +0100

    many to many

--- resources/views/guest/categories/show.blade.php
+++ resources/views/guest/categories/show.blade.php
@@ -1,23 +1,21 @@
+
 @extends('layouts.app')
 
 @section('content')
-
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>All Posts</h1>
+                <h1>Categoria: {{ $category->name }}</h1>
                 <ul>
-                    @foreach ($posts as $post)
+                    @foreach ($category->posts as $post)
                         <li>
                             <a href="{{ route('posts.show', ['slug' => $post->slug]) }}">
-                              {{ $post->title }}
-                          </a>
+                                {{ $post->title }}
+                            </a>
                         </li>
-
                     @endforeach
                 </ul>
             </div>
         </div>
     </div>
-
 @endsection
--- resources/views/guest/posts/show.blade.php
+++ resources/views/guest/posts/show.blade.php
@@ -1,16 +1,31 @@
-
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
-      <div class="row">
-          <div class="col-12">
-              <h1>{{ $post->title }}</h1>
-              <div>
-                  {{ $post->content }}
-              </div>
-              <p>Categoria: {{ $post->category ? $post->category->name : '-' }}</p>
-          </div>
-      </div>
-  </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>{{ $post->title }}</h1>
+                <div>
+                    {{ $post->content }}
+                </div>
+                <p>Categoria:
+                    @if ($post->category)
+                        <a href="{{ route('categories.show', ['slug' => $post->category->slug]) }}">
+                            {{ $post->category->name }}
+                        </a>
+                    @else
+                        -
+                    @endif
+
+                </p>
+                {{-- <p>Tags:
+                    @forelse ($post->tags as $tag)
+                        {{ $tag->name }}{{ !$loop->last ? ',' : '' }}
+                    @empty
+                        -
+                    @endforelse
+                </p> --}}
+            </div>
+        </div>
+    </div>
 @endsection
