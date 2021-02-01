09dcc52eb52caa467b30aa9d3ae5fe2bb45a53e0
Mikko Paulo Guevarra <emphigie10@gmail.com>
2021-02-01 17:29:14 +0100

    edit validation

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -26,10 +26,16 @@
                 <div class="form-group">
                     <label>Titolo</label>
                     <input type="text" name="title" class="form-control" placeholder="Inserisci il titolo" value="{{old('title')}}" required>
+                    @error('title')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label>Contenuto</label>
                     <textarea name="content" class="form-control" rows="10" placeholder="Inizia a scrivere qualcosa..." required>{{old('content')}}</textarea>
+                    @error('content')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label>Categoria</label>
@@ -41,17 +47,24 @@
                             </option>
                         @endforeach
                     </select>
+                    @error('category_id')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <p>Seleziona i tag:</p>
                     @foreach ($tags as $tag)
                         <div class="form-check">
-                            <input name="tags[]" class="form-check-input" type="checkbox" value="{{ $tag->id }}" {{ in_array($tag->id, old('tags', [])) ? 'checked=checked' : '' }}>
+                            <input name="tags[]" class="form-check-input" type="checkbox" value="{{ $tag->id }}"
+                            {{ in_array($tag->id, old('tags', [])) ? 'checked=checked' : '' }}>
                             <label class="form-check-label">
                                 {{ $tag->name }}
                             </label>
                         </div>
                     @endforeach
+                    @error('tags')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <button type="submit" class="btn btn-success">
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -15,33 +15,51 @@
                 @method('PUT')
                 <div class="form-group">
                     <label>Titolo</label>
-                    <input type="text" name="title" class="form-control" placeholder="Inserisci il titolo" value="{{ $post->title }}" maxlength="255" required>
+                    <input type="text" name="title" class="form-control" placeholder="Inserisci il titolo" value="{{ old('title', $post->title) }}" required>
+                    @error('title')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label>Contenuto</label>
-                    <textarea name="content" class="form-control" rows="10" placeholder="Inizia a scrivere qualcosa..." required>{{ $post->content }}</textarea>
+                    <textarea name="content" class="form-control" rows="10" placeholder="Inizia a scrivere qualcosa..." required>{{ old('content', $post->content) }}</textarea>
+                    @error('content')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label>Categoria</label>
                     <select class="form-control" name="category_id">
                         <option value="">-- seleziona categoria --</option>
                         @foreach ($categories as $category)
-                            <option value="{{ $category->id }}" {{ $category->id == $post->category_id ? 'selected=selected' : '' }}>
+                            <option value="{{ $category->id }}" {{ $category->id == old('category_id', $post->category_id) ? 'selected=selected' : '' }}>
                                 {{ $category->name }}
                             </option>
                         @endforeach
                     </select>
+                    @error('category_id')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <p>Seleziona i tag:</p>
                     @foreach ($tags as $tag)
                         <div class="form-check">
-                            <input name="tags[]" class="form-check-input" type="checkbox" value="{{ $tag->id }}">
+                            @if($errors->any())
+                                <input name="tags[]" class="form-check-input" type="checkbox" value="{{ $tag->id }}"
+                                {{ in_array($tag->id, old('tags', [])) ? 'checked=checked' : '' }}>
+                            @else
+                                <input name="tags[]" class="form-check-input" type="checkbox" value="{{ $tag->id }}"
+                                {{ $post->tags->contains($tag) ? 'checked=checked' : '' }}>
+                            @endif
                             <label class="form-check-label">
                                 {{ $tag->name }}
                             </label>
                         </div>
                     @endforeach
+                    @error('tags')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <button type="submit" class="btn btn-success">
